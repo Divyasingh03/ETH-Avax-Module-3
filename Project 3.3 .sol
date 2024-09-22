40db5761dc0c29eb4d00e33bb6ab4dacb0fc5be6
Divyasingh03 <divya954.singh@gmail.com>
2024-09-22 21:14:43 +0530

    Update Project 3.3 .sol

--- Project 3.3 .sol
+++ Project 3.3 .sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.26;
 
-import "ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Mtoken is ERC20 {
-    address owner = msg.sender;
-    modifier onlyOwner { 
-        require (owner == msg.sender, "only owner can access");
-        _;
+contract MyToken is ERC20 {
+    address public founder;
+
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol) {
+        founder = msg.sender;
+        _mint(founder, _initialSupply ); // Use the ERC20 mint function and set initial supply
     }
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) onlyOwner{
-        
-        _mint(msg.sender, 100 * 10**uint(decimals()));
+
+    function mint(address recipient, uint256 amount) public {
+        require(msg.sender == founder, "Unauthorized: Only the founder can mint tokens");
+        _mint(recipient, amount);
     }
-    function token_transfer(address add,uint256 value) external  {
-        _transfer(msg.sender, add, value);
-    }
-    function token_burn(address add, uint256 value) external {
-        burn(add, value);
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
     }
 }
